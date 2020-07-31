1b58d4eb0da6ba5c3025b9bfadacadb47ff7b387
Behdad Nemati <behdad.nemati1@gmail.com>
2020-07-31 14:25:52 +0430

    added Alerts Controller in Common/Alerts

---
+++ Common/Alerts/AlertsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Common.Alerts
+{
+    public class AlertsController : Controller
+    {
+        public enum alerts
+        { 
+            Success,
+            Warning,
+            Error,
+            Info
+        }
+
+        public void SendAlert(string key,alerts alerts)
+        {
+            switch (alerts)
+            {
+                case alerts.Success:
+                    TempData["GlobalSuccess"] = Alerts.Success[key];
+                    break;
+                case alerts.Warning:
+                    TempData["GlobalWarning"] = Alerts.Warning[key];
+                    break;
+                case alerts.Error:
+                    TempData["GlobalError"] = Alerts.Error[key];
+                    break;
+                case alerts.Info:
+                    TempData["GlobalInfo"] = Alerts.Info[key];
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

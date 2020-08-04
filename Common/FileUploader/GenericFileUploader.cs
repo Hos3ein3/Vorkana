b333614cb78c98cc99d98a9d57ce5191254cd82b
Behdad Nemati <behdad.nemati1@gmail.com>
2020-08-04 18:43:13 +0430

    added xml documentation for FileUploader

--- Common/FileUploader/GenericFileUploader.cs
+++ Common/FileUploader/GenericFileUploader.cs
@@ -12,7 +12,16 @@ namespace Common.FileUploader
 {
     public static class GenericFileUploader
     {
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="path">Location to save file on hard drive.</param>
+        /// <param name="maxSize">Maximum allowed file size in bytes.</param>
+        /// <param name="fileExtensions">Allowed file extensions (separate with commas and must be valid image formats).</param>
+        /// <param name="width">Resize image width.</param>
+        /// <param name="height">resize image height</param>
+        /// <returns></returns>
         public static async Task UploadImage(IFormFile file, string path, int maxSize = 0, string fileExtensions = "", int width = 0, int height = 0)
         {
             //fileExtensions = String.IsNullOrEmpty(fileExtensions) ? Extensions[FileType.Picture] : fileExtensions;
@@ -29,7 +38,7 @@ namespace Common.FileUploader
                     if (fileType == "image")
                     {
                         //await UploadFile(file, stream, path, maxSize);
-                        if (file.Length > 0 && (file.Length < maxSize || maxSize == 0))
+                        if (file.Length > 0 && (file.Length < maxSize || maxSize == -1))
                         {
                             try
                             {
@@ -57,6 +66,14 @@ namespace Common.FileUploader
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="path">Location to save file on hard drive.</param>
+        /// <param name="maxSize">Maximum allowed file size in bytes.</param>
+        /// <param name="fileExtensions">Allowed file extensions (separate with commas and must be valid video formats).</param>
+        /// <returns></returns>
         public static async Task UploadVideo(IFormFile file, string path, int maxSize = 0, string fileExtensions = "")
         {
 
@@ -81,6 +98,14 @@ namespace Common.FileUploader
             throw new Exception("Invalid video file extension");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="path">Location to save file on hard drive.</param>
+        /// <param name="maxSize">Maximum allowed file size in bytes.</param>
+        /// <param name="fileExtensions">Allowed file extensions (separate with commas and must be valid audio formats).</param>
+        /// <returns></returns>
         public static async Task UploadAudio(IFormFile file, string path, int maxSize = 0, string fileExtensions = "")
         {
             //fileExtensions = String.IsNullOrEmpty(fileExtensions) ? Extensions[FileType.Audio] : fileExtensions;
@@ -107,14 +132,15 @@ namespace Common.FileUploader
         {
 
         }
-        public static async Task UploadArchive(IFormFile zipFiles, string path, int maxSize = 0)
+        public static async Task UploadArchive(IFormFile zipFiles, string path, int maxSize = 0, bool allowExecutables = false)
         {
+
         }
 
 
         private static async Task Upload(IFormFile file, Stream fileStream, string path, int maxSize = 0) // do not fucking check extension here 
         {
-            if (file.Length > 0 && (file.Length < maxSize || maxSize == 0))
+            if (file.Length > 0 && (file.Length < maxSize || maxSize == -1))
             {
                 try
                 {
@@ -149,7 +175,7 @@ namespace Common.FileUploader
         {
             [FileType.Picture] = "jpeg,jpg,png,bmp,gif,svg",
             [FileType.Video] = "mp4,3gp,mkv,wmv,avi",
-            [FileType.Archive] = "zip,rar,iso,tar,tar.gz",
+            [FileType.Archive] = "zip,rar,iso,tar,tar.gz,7z,",
             [FileType.Document] = "doc,docx,pdf,txt,html,hml,xls,xlsx,ppt,pptx",
             [FileType.Audio] = "mp3,voc,m4a,wav"
         };

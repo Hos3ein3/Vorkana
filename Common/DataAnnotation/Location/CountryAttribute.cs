a6bdf60b20d3c91aabd9e35d7c483c6e5d1623f3
Hossein Behzadi <behzadi.hosein@yahoo.com>
2020-08-04 12:51:25 +0430

    NewDataAnnotation

--- Common/DataAnnotation/Location/CountryAttribute.cs
+++ Common/DataAnnotation/Location/CountryAttribute.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Common.DataAnnotation
 {
-    class StartDateDataAnnotationAttribute
+    class CountryAttribute
     {
     }
 }
--- Common/DataAnnotation/Social/PhoneNumberAttribute.cs
+++ Common/DataAnnotation/Social/PhoneNumberAttribute.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Common.DataAnnotation
 {
-    class EmailDataAnnotationAttribute
+    public class PhoneNumberAttribute : RequiredAttribute
     {
+
     }
 }
